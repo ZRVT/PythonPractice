732ad2fd4232d52a4d9e1e6d9a553a84365eaebc
ZRVT <144631997+ZRVT@users.noreply.github.com>
2025-02-09 15:04:22 +0800

    update to repository

---
+++ Master Python and SQL for Data Processing/start.py
@@ -0,0 +1,11 @@
+string = "Hello world"
+integer = 4
+integerConverted = float(4) 
+float = 3.14
+floatConverted = int(3.14)
+boolean = True
+
+variableOfInterest = floatConverted
+
+print(variableOfInterest, type(variableOfInterest))
+
